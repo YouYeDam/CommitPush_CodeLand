f3b4aef3c53cd56c1706c17f93f043b278d23a08
YouYedam <youyedam@naver.com>
2024-04-02 17:55:16 +0900

    플레이어 이동, 점프, 사다리 타기 애니메이션 구현

--- Assets/Scripts/Create Player/ChangeCharacter.cs
+++ Assets/Scripts/Create Player/ChangeCharacter.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeSprite : MonoBehaviour
+public class ChangeCharacter : MonoBehaviour
 {
     public Sprite NewSprite; // 변경할 플레이어 스프라이트를 에디터에서 할당
     [SerializeField] public string TargetTag;
     public SpriteRenderer[] RenderersToActivate; // 활성화할 스프라이트 렌더러 배열
     public SpriteRenderer[] RenderersToDeactivate; // 비활성화할 스프라이트 렌더러 배열
+    public RuntimeAnimatorController NewAnimatorController; // 변경할 애니메이터 컨트롤러
 
     public void SpriteChange()
     {
         // 태그로 대상 오브젝트를 찾음
-        GameObject targetObject = GameObject.FindGameObjectWithTag(TargetTag);
-        if (targetObject != null)
+        GameObject TargetObject = GameObject.FindGameObjectWithTag(TargetTag);
+        if (TargetObject != null)
         {
-            SpriteRenderer spriteRenderer = targetObject.GetComponent<SpriteRenderer>();
-            if (spriteRenderer != null)
+            SpriteRenderer SpriteRenderer = TargetObject.GetComponent<SpriteRenderer>();
+            if (SpriteRenderer != null)
             {
-                spriteRenderer.sprite = NewSprite; // 스프라이트 변경
+                SpriteRenderer.sprite = NewSprite; // 스프라이트 변경
             }
         }
     }
@@ -35,6 +36,20 @@ public class ChangeSprite : MonoBehaviour
         foreach (SpriteRenderer Renderer in RenderersToDeactivate)
         {
             Renderer.enabled = false;
+        }
+    }
+    
+    public void ChangeAnimatorController()
+    {
+        // 태그로 대상 오브젝트를 찾음
+        GameObject targetObject = GameObject.FindGameObjectWithTag(TargetTag);
+        if (targetObject != null)
+        {
+            Animator Animator = targetObject.GetComponent<Animator>();
+            if (Animator != null)
+            {
+                Animator.runtimeAnimatorController = NewAnimatorController; // 애니메이터 컨트롤러 변경
+            }
         }
     }
 }
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+Vector2 MoveInput;
+Rigidbody2D MyRigidbody;
+Animator MyAnimator;
+CapsuleCollider2D MyCapsuleCollider;
+BoxCollider2D MyFeetCollider;
+float StartGravity;
+[SerializeField] float RunSpeed = 10f;
+[SerializeField] float JumpSpeed = 5f;
+[SerializeField] float ClimbSpeed = 5f;
+
+void Start()
+{
+    MyRigidbody = GetComponent<Rigidbody2D>();
+    MyAnimator = GetComponent<Animator>();
+    MyCapsuleCollider = GetComponent<CapsuleCollider2D>();
+    MyFeetCollider = GetComponent<BoxCollider2D>();
+
+    StartGravity = MyRigidbody.gravityScale;
+}
+
+void Update()
+{
+    Walk();
+    CheckWalk();
+    ClimbLadder();
+    FlipSprite();
+}
+void OnMove(InputValue Value) {
+    MoveInput = Value.Get<Vector2>();
+}
+
+void Walk() { //플레이어 좌우이동
+    bool PlayerHasHorizontalSpeed = Mathf.Abs(MyRigidbody.velocity.x) > Mathf.Epsilon;
+
+    Vector2 PlayerVelocity = new Vector2 (MoveInput.x * RunSpeed, MyRigidbody.velocity.y); // 현재의 속도인 y가 무엇이든 동일한 속도를 유지하라는 의미
+    MyRigidbody.velocity = PlayerVelocity;
+    
+    MyAnimator.SetBool("IsWalking", PlayerHasHorizontalSpeed);
+} 
+
+void CheckWalk() { //걷기 애니메이션 체크
+    bool IsOnGround = MyFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    bool IsOnLadder = MyFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ladder"));
+    if (!IsOnGround && !IsOnLadder) {
+        MyAnimator.SetBool("IsWalking", false);
+        MyAnimator.SetBool("IsJumping", true);
+    }
+    else {
+        MyAnimator.SetBool("IsJumping", false);   
+    }
+}
+void FlipSprite() { //플레이어 좌우반전
+    bool PlayerHasHorizontalSpeed = Mathf.Abs(MyRigidbody.velocity.x) > Mathf.Epsilon; // abs: 절대값 반환, Epsilon: 0에 가까운 아주 작은 값 반환
+
+    if (PlayerHasHorizontalSpeed) {
+        transform.localScale = new Vector2 (Mathf.Sign(MyRigidbody.velocity.x), 1f); // 0보다 크면1, 작으면 -1 반환
+    }
+}
+
+void OnJump(InputValue Value) { //플레이어 점프
+    bool IsOnGround = MyFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    if (!IsOnGround) {
+        return;
+    }
+    if (Value.isPressed) {
+        MyRigidbody.velocity += new Vector2 (0f, JumpSpeed);
+    }
+}
+
+void ClimbLadder() { //플레이어 사다리 타기
+    bool IsOnLadder = MyFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ladder"));
+
+    if (!IsOnLadder) {
+        MyAnimator.SetBool("IsClimbing", false);
+        MyAnimator.SetBool("IsClimbingIdle", false);
+        MyRigidbody.gravityScale = StartGravity;
+        return;
+    }
+
+    float LadderSpeed = MoveInput.y * ClimbSpeed;
+    MyRigidbody.gravityScale = 0f;
+    MyRigidbody.velocity = new Vector2(MyRigidbody.velocity.x, LadderSpeed);
+
+    bool playerHasVerticalSpeed = Mathf.Abs(MyRigidbody.velocity.y) > Mathf.Epsilon;
+    MyAnimator.SetBool("IsClimbing", playerHasVerticalSpeed);
+    MyAnimator.SetBool("IsClimbingIdle", !playerHasVerticalSpeed);
+    MyAnimator.SetFloat("LadderSpeed", Mathf.Abs(LadderSpeed)); // 사다리 속도를 Animator에 전달
+}
+
+}
+
+
