a517674a5f9f3c8c5f2e0703dc0529dff508ad0c
bachohyeonho <133359989+bachohyeonho@users.noreply.github.com>
2024-05-16 16:08:55 +0900

    코드 정리

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -141,32 +141,25 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    // 사다리에서 점프를 가능하게 하기 위한 코루틴. 1. 현재 플레이어의 y 위치를 눈에 띄지 않을 만큼 올리고(사다리 rigidbody에 걸리는 현상 해결) 2. 원래대로 velocity에 벡터값 추가. 
-    IEnumerator MagicJump(float modified_jump_speed)
-    {
-        transform.position = new Vector2(transform.position.x, transform.position.y + 0.15f);
-        yield return new WaitForSeconds(0.001f);
-        MyRigidbody.velocity += new Vector2(0f, modified_jump_speed);
-    }
+    // 
+    // void OnJump(InputValue Value)
+    // { 
+    //     if (IsAlive == false || !PlayerManager.CanInput)
+    //     {
+    //         return;
+    //     }
+    //     bool IsOnGround = MyCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    //     bool IsSteppingLadder = MyCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")) && !MyBoxColliders[1].IsTouchingLayers(LayerMask.GetMask("Ladder"));
+    //     if (!IsOnGround & !IsSteppingLadder)
+    //     {
+    //         return;
+    //     }
+    //     if (Value.isPressed)
+    //     {
+    //         StartCoroutine(MagicJump(JumpSpeed));
+    //     }
 
-    void OnJump(InputValue Value)
-    { //플레이어 점프
-        // if (IsAlive == false || !PlayerManager.CanInput)
-        // {
-        //     return;
-        // }
-        // bool IsOnGround = MyCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
-        // bool IsSteppingLadder = MyCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")) && !MyBoxColliders[1].IsTouchingLayers(LayerMask.GetMask("Ladder"));
-        // if (!IsOnGround & !IsSteppingLadder)
-        // {
-        //     return;
-        // }
-        // if (Value.isPressed)
-        // {
-        //     StartCoroutine(MagicJump(JumpSpeed));
-        // }
-
-    }
+    // }
 
 
     void ClimbLadder()
