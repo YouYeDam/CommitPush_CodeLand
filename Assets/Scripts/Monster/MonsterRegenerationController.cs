cc2233e7ae662f1773df40e365bc03257945a9e6
YouYeDam <youyedam@naver.com>
2024-06-09 23:12:58 +0900

    아이템 사라지는 버그 해결

--- Assets/Scripts/Monster/MonsterRegenerationController.cs
+++ Assets/Scripts/Monster/MonsterRegenerationController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MonsterRegenerationController : MonoBehaviour
 {
     [SerializeField] float DelayTime = 10f; // 몬스터 재생성까지 걸리는 시간
-    [SerializeField] float ColliderDisableDelay = 1f; // 몬스터에게 boxcollider가 생길때 까지의 시간
+    [SerializeField] float CanAttackDelay = 1f; // 몬스터 공격 기능 대기 시간
     BasicMonsterMovement BasicMonsterMovement;
     public void RegenerateMonster(GameObject MonsterObject, Vector3 MonsterPosition, Quaternion MonsterRotation) { // 몬스터 생성 함수
         GameObject MonsterInstance = Instantiate(MonsterObject, MonsterPosition, MonsterRotation);
@@ -22,7 +22,7 @@ public class MonsterRegenerationController : MonoBehaviour
         MonsterInstance.SetActive(true);
         BasicMonsterMovement.CanTriggerDamage = false;
 
-        yield return new WaitForSeconds(ColliderDisableDelay);
+        yield return new WaitForSeconds(CanAttackDelay);
         BasicMonsterMovement.CanTriggerDamage = true;
     }
 }
